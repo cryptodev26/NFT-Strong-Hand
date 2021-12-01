a37facc905fdfc08d4ac0f00d44579dff5739010
J-Gambler <antonovserg943@gmail.com>
2021-12-01 22:17:20 +0300

    control header toggler

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,23 @@
 import React from "react";
-import { Container, Row, Col } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import { Link } from "react-router-dom";
 
 class Header extends React.Component {
     constructor () {
         super()
         this.state =  {
-            route : window.location.pathname
+            route : window.location.pathname,
+            show : false,
         }
+        this.toggle = this.toggle.bind(this)
+    }
+
+    toggle (e) {
+        this.setState({
+            show : !this.state.show
+        })
+        const toggler = document.getElementsByClassName('toggle-collapse')[0]
+        toggler.style.display = (this.state.show ? 'none' : 'block')
     }
 
     render () {
@@ -22,17 +32,22 @@ class Header extends React.Component {
                     <img src={require('../assets/img/diamond.svg').default} />
                 </div>
                 <div className="links">
-                    <ul className="links">
-                        <li>
-                            <Link to="/home" className={this.state.route == '/home' ? 'active' : ''}>HOME</Link>
-                        </li>
-                        <li>
-                            <Link to="/demo" className={this.state.route == '/demo' ? 'active' : ''}>DEMO</Link>
-                        </li>
-                        <li>
-                            <Link to="/contact" className={this.state.route == '/contact' ? 'active' : ''}>CONTACT</Link>
-                        </li>
-                    </ul>
+                    <div className="toggle-collapse">
+                        <ul>
+                            <li>
+                                <Link to="/home" className={this.state.route == '/home' ? 'active' : ''}>HOME</Link>
+                            </li>
+                            <li>
+                                <Link to="/demo" className={this.state.route == '/demo' ? 'active' : ''}>DEMO</Link>
+                            </li>
+                            <li>
+                                <Link to="/contact" className={this.state.route == '/contact' ? 'active' : ''}>CONTACT</Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <Button variant="secondary" onClick={this.toggle} className="collapse-button">
+                        <span className="navbar-toggler-icon"></span>
+                    </Button>
                 </div>
             </div>
         );
